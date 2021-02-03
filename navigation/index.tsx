ec048cbfc60a03ff00116a413986ec3a1e12f66e
Shubham Dhage <shubhamd@rigbot.com>
2021-02-03 16:54:17 +0530

    expo react native setup

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -26,7 +26,11 @@ const Stack = createStackNavigator<RootStackParamList>();
 
 function RootNavigator() {
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
+    <Stack.Navigator screenOptions={{
+      headerStyle: {
+        backgroundColor: "red",
+      }
+    }}>
       <Stack.Screen name="Root" component={BottomTabNavigator} />
       <Stack.Screen name="NotFound" component={NotFoundScreen} options={{ title: 'Oops!' }} />
     </Stack.Navigator>
